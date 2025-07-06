fb211bc6782c35a16d10d3750c299ef3df83f63c
aquacat1220 <aquacat1220@gmail.com>
2025-07-06 10:57:16 +0000

    feat: change ghost agent assigning logic

--- pacman/wrappers.py
+++ pacman/wrappers.py
@@ -1,6 +1,6 @@
 from gymnasium import Env
 import numpy as np
-from typing import Any
+from typing import Any, Callable
 from .env import PacmanEnv
 from .agents import GhostAgentBase
 
@@ -8,13 +8,16 @@ from .agents import GhostAgentBase
 class GymWrapper(
     Env[tuple[np.ndarray[Any, np.dtype[np.uint8]], tuple[int, int], int], int]
 ):
-    def __init__(self, env: PacmanEnv, ghosts: dict[str, GhostAgentBase]):
+    def __init__(self, env: PacmanEnv, ghost_builder: Callable[[str], GhostAgentBase]):
         self.env = env
         self._last_observations: (
             dict[str, tuple[np.ndarray[Any, np.dtype[np.uint8]], tuple[int, int], int]]
             | None
         ) = None
-        self._ghosts = ghosts
+        self._ghosts: dict[str, GhostAgentBase] = {}
+        for ghost_name in self.env.ghosts:
+            ghost = ghost_builder(ghost_name)
+            self._ghosts[ghost_name] = ghost
 
     def step(self, action: int) -> tuple[
         tuple[np.ndarray[Any, np.dtype[np.uint8]], tuple[int, int], int],
